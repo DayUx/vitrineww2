c2a45cf2f21ea8648f7658a000572c7f5da4ef8c
DayUx <61156794+DayUx@users.noreply.github.com>
2020-04-27 18:53:59 +0200

    Add files via upload

--- global.js
+++ global.js
@@ -1,59 +1,62 @@
-
-
 function transition() {
-    
-    
-    var elmnt = document.getElementById("section2");
+
+
+  var elmnt = document.getElementById("section2");
   document.getElementById("transition").classList.remove('not-visible')
-    setTimeout(() => {   document.getElementById("body").style.overflowY = "visible";
-    elmnt.scrollIntoView({behavior: "smooth", inline: "nearest"})
-     document.getElementById("transition").classList.add('not-visible')                 
-                      
-                      
-                      ;}, 1000);
-}
-var a=1;
+  setTimeout(() => {
+    document.getElementById("body").style.overflowY = "visible";
+    elmnt.scrollIntoView({
+      behavior: "smooth",
+      inline: "nearest"
+    })
+    document.getElementById("transition").classList.add('not-visible')
 
-function onClickMenu(){
-    
-	document.getElementById("menu").classList.toggle("change");
-    
-    document.getElementById("top").classList.toggle("change");
-     document.getElementById("top").classList.toggle("show");
-	
-}
 
-function onglet1(){
-    
-    document.getElementById("onglet").classList.toggle("change");
-    
-    
+    ;
+  }, 1000);
 }
-function onglet2(){
-    
-    document.getElementById("ongletdeux").classList.toggle("change");
-    
-    
+var a = 1;
+
+function onClickMenu() {
+
+  document.getElementById("menu").classList.toggle("change");
+
+  document.getElementById("onglets").classList.toggle("change");
+  document.getElementById("onglets").classList.toggle("show");
+  document.getElementById("bordure").classList.toggle("change");
 }
 
-function onglet3(){
-    
-    document.getElementById("onglettrois").classList.toggle("change");
-    
-    
+function onglet1() {
+
+  document.getElementById("onglet").classList.toggle("change");
+
+  document.getElementById("bordure").classList.toggle("change");
 }
 
-function line1(){
-    
-    document.getElementById("borderline").classList.toggle("change");
-    
-    
+function onglet2() {
+
+  document.getElementById("ongletdeux").classList.toggle("change");
+
+  document.getElementById("bordure").classList.toggle("change");
 }
-function line2(){
-    
-    document.getElementById("borderlinedeux").classList.toggle("change");
-    
-    
+
+function onglet3() {
+
+  document.getElementById("onglettrois").classList.toggle("change");
+
+  document.getElementById("bordure").classList.toggle("change");
+}
+
+function line1() {
+
+  document.getElementById("borderline").classList.toggle("change");
+
+}
+
+function line2() {
+
+  document.getElementById("borderlinedeux").classList.toggle("change");
+
 }
 
 
@@ -62,11 +65,11 @@ let observer = new IntersectionObserver(function (observables) {
     // L'élément devient visible
     if (observable.intersectionRatio > 0.5) {
       observable.target.classList.remove('not-visible')
-      
-    }else {
-        observable.target.classList.add('not-visible')
-        
-        
+
+    } else {
+      observable.target.classList.add('not-visible')
+
+
     }
   })
 }, {
