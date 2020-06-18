70e8b2e3ae24102c66ee82f79c43c056bd78e9e2
DayUx <61156794+DayUx@users.noreply.github.com>
2020-06-18 17:27:32 +0200

    Add files via upload

--- global.js
+++ global.js
@@ -1,29 +1,37 @@
 function transition() {
 
 
-  var elmnt = document.getElementById("section2");
-  document.getElementById("transition").classList.remove('not-visible')
-  setTimeout(() => {
-    document.getElementById("body").style.overflowY = "visible";
-    elmnt.scrollIntoView({
-      behavior: "smooth",
-      inline: "nearest"
-    })
-    document.getElementById("transition").classList.add('not-visible')
+    var elmnt = document.getElementById("section2");
+    document.getElementById("transition").classList.remove('not-visible')
+    setTimeout(() => {
+        document.getElementById("body").style.overflowY = "visible";
+        elmnt.scrollIntoView({
+            behavior: "smooth",
+            inline: "nearest"
+        })
+        document.getElementById("transition").classList.add('not-visible')
 
 
-    ;
-  }, 1000);
+        ;
+    }, 1000);
 }
 var a = 1;
 
 function onClickMenu() {
 
-  document.getElementById("menu").classList.toggle("change");
 
-  document.getElementById("onglets").classList.toggle("change");
-  document.getElementById("onglets").classList.toggle("show");
-  document.getElementById("bordure").classList.toggle("change");
+    document.getElementById("onglets").classList.toggle("change");
+    document.getElementById("onglets").classList.toggle("show");
+    document.getElementById("titre").classList.toggle("change");
+    document.getElementById("bordure").classList.toggle("change");
+    document.getElementById("container").classList.toggle("change");
+        document.getElementById("infoUn").classList.toggle("change");
+        document.getElementById("infoDeux").classList.toggle("change");
+            document.getElementById("close").classList.toggle("change");
+
+
+    
+
 }
 
 
@@ -32,31 +40,31 @@ function onClickMenu() {
 
 function onglet1() {
 
-  document.getElementById("onglet").classList.toggle("change");
+    document.getElementById("onglet").classList.toggle("change");
 
-  document.getElementById("bordure").classList.toggle("change");
+    document.getElementById("bordure").classList.toggle("change");
 }
 
 function onglet2() {
 
-  document.getElementById("ongletdeux").classList.toggle("change");
+    document.getElementById("ongletdeux").classList.toggle("change");
 
-  document.getElementById("bordure").classList.toggle("change");
+    document.getElementById("bordure").classList.toggle("change");
 }
 
 
 
 function onglet3_2() {
-     document.getElementById("onglettrois").classList.toggle("change");
-    
-    document.getElementById("onglets").classList.toggle("change");   
+    document.getElementById("onglettrois").classList.toggle("change");
+
+    document.getElementById("onglets").classList.toggle("change");
 }
 
 
 function onglet1_2() {
-     document.getElementById("onglet").classList.toggle("change");
-    
-    document.getElementById("onglets").classList.toggle("change");   
+    document.getElementById("onglet").classList.toggle("change");
+
+    document.getElementById("onglets").classList.toggle("change");
 }
 
 
@@ -64,43 +72,43 @@ function onglet1_2() {
 
 function onglet3() {
 
-  document.getElementById("onglettrois").classList.toggle("change");
+    document.getElementById("onglettrois").classList.toggle("change");
 
-  document.getElementById("bordure").classList.toggle("change");
+    document.getElementById("bordure").classList.toggle("change");
 }
 
 function line1() {
 
-  document.getElementById("borderline").classList.toggle("change");
+    document.getElementById("borderline").classList.toggle("change");
 
 }
 
 function line2() {
 
-  document.getElementById("borderlinedeux").classList.toggle("change");
+    document.getElementById("borderlinedeux").classList.toggle("change");
 
 }
 
 
 let observer = new IntersectionObserver(function (observables) {
-  observables.forEach(function (observable) {
-    // L'élément devient visible
-    if (observable.intersectionRatio > 0.5) {
-      observable.target.classList.remove('not-visible')
+    observables.forEach(function (observable) {
+        // L'élément devient visible
+        if (observable.intersectionRatio > 0.5) {
+            observable.target.classList.remove('not-visible')
 
-    } else {
-      observable.target.classList.add('not-visible')
+        } else {
+            observable.target.classList.add('not-visible')
 
 
-    }
-  })
+        }
+    })
 }, {
-  threshold: [0.5]
+    threshold: [0.5]
 });
 
 // On observe nos éléments
 let items = document.querySelectorAll('#para1,#para2,#para3')
 items.forEach(function (item) {
-  item.classList.add('not-visible')
-  observer.observe(item)
+    item.classList.add('not-visible')
+    observer.observe(item)
 })
